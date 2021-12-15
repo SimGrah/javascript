9f53cc9f65e8078896ef8abf750e4b0b4f035dac
Simon Grahn <simme.97@live.se>
2021-12-15 20:18:14 +0100

    Nästan färdig med del 1 av den andra individuella uppgiften.

---
+++ Individuell2/Del1/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000);
+console.log("Kör servern på localhost:3000");
+
+app.use(express.static("publik"));
+
+
+let fs = require("fs"); 
+app.get("/", (req, res) => {
+    fs.readFile("form.html", function(err, htmlData) {
+
+        fs.readFile("data.txt", function(err, textData) {
+            let htmlText = htmlData.toString();
+            let textText = textData.toString();
+            let output = htmlText.replace(/Formulär/, textText);
+            console.log(output);
+            res.send(output);
+        });
+    });
+
+});
+
+app.use(express.urlencoded({extended: true}));
+app.post("/", (req, res) => {
+    let input = "Namn: " + req.body.name.replace(/</g, "&lt;") + "<br> \n" + 
+    "email: " + req.body.email.replace(/</g, "&lt;") + "<br> <br> <br>\n\n";
+    fs.appendFile("data.txt", input, function(err) {
+        if (err) throw err;
+
+        fs.readFile("form.html", function(err, htmlData) {
+            fs.readFile("data.txt", function(err, textData) {
+                let htmlText = htmlData.toString();
+                let textText = textData.toString();
+                let output = htmlText.replace(/Formulär/, textText);
+                console.log(output);
+                res.send(output);
+            });
+        });
+    });
+});
+
+
