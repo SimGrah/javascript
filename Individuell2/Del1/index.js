23213c4538134cb6f4add48c3e07c4393375aa50
Simon Grahn <simme.97@live.se>
2021-12-16 17:07:18 +0100

    Gjorde klart formuläret och det fungerar som det ska

--- Individuell2/Del1/index.js
+++ Individuell2/Del1/index.js
@@ -24,8 +24,7 @@ app.get("/", (req, res) => {
 
 app.use(express.urlencoded({extended: true}));
 app.post("/", (req, res) => {
-    let input = "Namn: " + req.body.name.replace(/</g, "&lt;") + "<br> \n" + 
-    "email: " + req.body.email.replace(/</g, "&lt;") + "<br> <br> <br>\n\n";
+    let input = "Namn: " + req.body.name.replace(/</g, "&lt;") + "<br>\n" + "Email: " + req.body.email.replace(/</g, "&lt;") + "<br>\n" + "Phone: " + req.body.phone.replace(/</g, "&lt;") + "<br>\n" + "Comment: " + req.body.comment.replace(/</g, "&lt;") + "<br> <br> <br>\n\n";
     fs.appendFile("data.txt", input, function(err) {
         if (err) throw err;
 
