3245bf74d5c8070c1eca802fd756a64e0c023c49
Simon Grahn <simme.97@live.se>
2022-01-05 17:27:08 +0100

    Färdig chattapplikation med namn för användare

---
+++ Individuell2/Del2/script.js
@@ -0,0 +1,28 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('Vad heter du?')
+appendMessage('Du har joinat')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const message = messageInput.value
+  appendMessage(`Du: ${message}`)
+  socket.emit('send-chat-message', message)
+  messageInput.value = ''
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
